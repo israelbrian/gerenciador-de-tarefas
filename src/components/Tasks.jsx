ae5d99ef9832b4b10151fafad26eaf55fa5dbf9f
Israel <zdesenhos@gmail.com>
2024-11-12 23:24:23 -0300

    att

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,21 @@
 import { ChevronRightIcon, DeleteIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom"; // Importe useNavigate do react-router-dom
 
 function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+    const navigate = useNavigate();
+
+    function onSeeDetailsClick(task) {
+        const query = new URLSearchParams();
+        query.set("title", task.title);
+        query.set("description", task.description);
+        navigate(`/task?${query.toString()}`);
+    }
+
     return (
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow"> 
             {tasks.map((task) => (
                 <li key={task.id} className="flex gap-2">
+                    
                     <button 
                     onClick={() => onTaskClick(task.id)} 
                     className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && "line-through"}`}>
@@ -12,7 +23,8 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
                         
                     </button>
                     
-                    <button className="bg-slate-400 p-2 rounded-md text-white">
+                    <button onClick={() => onSeeDetailsClick(task)}
+                    className="bg-slate-400 p-2 rounded-md text-white">
                         <ChevronRightIcon />
                     </button>
 
